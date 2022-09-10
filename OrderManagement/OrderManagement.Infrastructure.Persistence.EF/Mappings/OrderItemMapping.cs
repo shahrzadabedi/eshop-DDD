5eed03c39e11d88e16e99215c5d2280f52174d65
sh.abedi <shahrzad.abedi@gmail.com>
2022-09-10 12:40:24 +0430

    Add Value object ProductId to OrderItem

---
+++ OrderManagement/OrderManagement.Infrastructure.Persistence.EF/Mappings/OrderItemMapping.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OrderManagement.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagement.Infrastructure.Persistence.EF.Mappings
+{
+    public class OrderItemMapping : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItem").HasKey(a => a.Id);
+            builder.OwnsOne(a => a.ProductId)
+                .Property(p => p.Value).HasColumnName("ProductId");
+        }
+    }
+}
---
+++ OrderManagement/OrderManagement.Infrastructure.Persistence.EF/Migrations/20220910080113_Add_ProductId_ToOrderItem.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderManagement.Infrastructure.Persistence.EF.Migrations
+{
+    public partial class Add_ProductId_ToOrderItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Value",
+                table: "OrderItem",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "OrderItem");
+        }
+    }
+}
